413db09eaef95d04200c0eda7177e39c6105d113
Nikita Istomin <nikita.istomin54@gmail.com>
2024-01-18 01:04:00 -0600

    Innitial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import "~/styles/globals.css";
+
+import type { Metadata } from "next";
+
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+
+import { TRPCReactProvider } from "~/trpc/react";
+
+export const metadata: Metadata = {
+  title: "Sailing Finder",
+
+  description: "A web app for finding closests sailing races to you.",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+        <TRPCReactProvider>{children}</TRPCReactProvider>
+      </body>
+    </html>
+  );
+}
